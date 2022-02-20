d732075340e9f904eea575a8cc7f151dab96d7df
fss <fss@qq.com>
2022-02-20 23:45:06 +0800

    第san次提交

--- src/hel.java
+++ src/hel.java
@@ -2,5 +2,6 @@ public class hel {
     public static void main(String[] args) {
         System.out.printf("hellot");
         System.out.println("dier ci tijiao");
+        System.out.println("di san ci xiugai");
     }
 }
